cc1bb20deb6feab3650153fb4d716b7606077434
vivianlorenaortiz <973@holbertonschool.com>
2019-09-26 05:09:23 +0200

    [adding file ]

---
+++ 9-nop_opcode.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "monty.h"
+
+/**
+ *nop - Function nop doesn’t do anything.
+ *@stack: Pointer to header of the stack.
+ *@line_number: Line number of the file
+ */
+
+void nop(stack_t **stack, unsigned in line_number)
+{
+	(void) stak;
+	(void) line_number;
+}
